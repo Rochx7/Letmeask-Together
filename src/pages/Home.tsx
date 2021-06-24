e3b24aedafd8ad116042acb46dc3d5643c2e5e9f
Guilherme Rocha <guirocha55@hotmail.com>
2021-06-24 16:05:43 -0300

    Aula 04 – Landing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -43,6 +43,11 @@ export function Home() {
             return;
         }
 
+        if (roomRef.val().endedAt) {
+            alert('room alredy closed.')
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`)
     }
 
